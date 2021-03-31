37f399171c9f4756cc49a87e1390d078e37fe98c
Orignal42 <rbarriquand@gmail.com>
2021-03-31 14:54:05 +0200

    ca marchegit add .git add .

--- class/Board.js
+++ class/Board.js
@@ -2,15 +2,28 @@ class Board{
     constructor(allPlayer){
         this.allPlayer = allPlayer 
         this.playerTurn = this.allPlayer[0]
+        this.tab = new Array(9).fill(null);
+    
+        
+        }
 
-    }
-    play(square){
+    
+    play(i){
         //joueur a qui c'est le tour joue
-        square.innerHTML = this.playerTurn.signe
+        event.target.innerHTML = this.playerTurn.signe
+        this.tab[i] = this.playerTurn.signe
+       console.log(this.tab)
+        if(this.findWinCon()){
+            alert(this.playerTurn.signe+" "+"a gagné")
+        }
+        if (!this.tab.includes(null && !this.findWinCon)){
+            alert("nulle")
+        }
 
-
+       
         this.switchTurn()
     }
+    
     switchTurn(){
         if ( this.playerTurn.signe == "X") {
             this.playerTurn = this.allPlayer[1]
@@ -18,5 +31,31 @@ class Board{
             this.playerTurn = this.allPlayer[0]
         }
     }
-
+    findWinCon() {
+        const winCon = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+  
+        for (const combination of winCon) {
+            const [a, b, c] = combination;
+  
+            if (this.tab[a] && (this.tab[a] === this.tab[b] && this.tab[a] === this.tab[c])) {
+                return combination;
+            }
+        }
+  
+        return null;
+    }
 }
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
 let squares =document.querySelectorAll('.square');
+let player1 = new Player('Romain', 'X')
+let player2 = new Player('Hamza', 'O')
+
+let game = new Board([player1, player2]);
+
+ss=0;
+s=00;
+m='0'+0;
+h='0'+0;
+j=0;
+window.status = "Chrono "+j+" jour(s) et " +h + ' : '  + m + ' : ' + s ; 
+ function Chrono() {
+    if (m==0) {m='00'}
+    if(h==0) {h='00'}
+    if (ss<10){ss='0'+ss;}
+    if (s<10){s='0'+s;}
+     window.status = "Chrono "+j+" jour(s) et " +h + ' : '  + m + ' : ' + s ; 
+    document.formu.heure.value =j+" jour(s) et "+h + ' : ' + m + ' : ' + s ;
+    document.formu.seco.value =ss;
+    s++;
+    ss++;
+    if (h==24){h='0'+0;j++;}
+    if (s==60){s=0;s=0; m++;if(m<10){m='0'+m;}}
+    if (m==60){m='0'+0;h++;if(h<10){h='0'+h;}}
+    chrono=window.setTimeout("Chrono();",1000);
+    }
 
 squares.forEach((square) =>{
     square.addEventListener('click',function(e){
-        game.play(e.target)
+        game.play(e.target.getAttribute('data-cell'))
+        event.target.style.pointerEvents= "none";
+        Chrono();
     })
 })
 
-    let player1 = new Player('Romain', 'X')
-    let player2 = new Player('Hamza', 'O')
+   
 
-    let game = new Board([player1, player2])
+
+
+
+  
+
+
+    
+
 
 
